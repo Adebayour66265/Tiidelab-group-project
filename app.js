c77c07d6f80fdf63308431377f39ba5cdfa02b19
Adebayour66265 <adebayour66265@gmail.com>
2023-05-13 23:48:51 +0100

    first commit

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ functionsetFormMessage(formElement, type, message){
 
 setFormMessage(loginForm, "success", "You're logged in!");
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createAccount");
 
-    document.querySelector("#linkCreateAccount").addEventListener("click",e =>{
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden");
     });
-    document.querySelector("#linkLogin").addEventListener("click", e =>{
+    document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden");
     });
-    loginForm.addEventListener("submit", e =>{
+    loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
         //Perform your AJAX/Fetch login
